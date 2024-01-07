7cb9d379562decebddfd987a4515d78e739e53d3
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2024-01-07 17:22:06 +0100

    initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,8 +33,7 @@ const Hero = () => {
                     </div>
 
                     <p className="bold-16 lg:bold-20 text-blue-70">
-                        198k
-                        <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
+                        198k <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
                     </p>
                 </div>
 
